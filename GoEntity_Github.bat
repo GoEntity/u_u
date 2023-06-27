1c1cd5a7d50d465813172c55b6fa6d786b89e788
GoEntity <goentity13@gmail.com>
2023-06-27 15:18:46 +0900

    auto update

--- GoEntity_Github.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd E:\Git\GoEntity_Github
-
-"C:\Go\bin\go.exe" run main.go
-
-"C:\Git\bin\git.exe" add .
-"C:\Git\bin\git.exe" commit -m "auto update"
-"C:\Git\bin\git.exe" push origin main
