3abe744be918c13ec657c20cf23f1f5bcd9283fa
GoEntity <goentity13@gmail.com>
2023-06-27 14:11:07 +0900

    ::auto commit

--- GoEntity_Github.bat
+++ GoEntity_Github.bat
@@ -1,17 +1,4 @@
 @echo off
-
-REM dirrr
-cd E:\Git\GoEntity_Github
-
-REM setup git config
-git config user.name "GoEntity"
-git config user.email "goentity13@gmail.com"
-
-REM run Go script
+cd /d E:\Git\GoEntity_Github
+set /p GoEntity_Github=<Secret_Token.txt
 go run main.go
-
-REM add, commit, and push changes
-git add -A
-for /f "tokens=2 delims==" %%I in ('wmic os get localdatetime /format:list') do set datetime=%%I
-git commit -m "Latest data: %datetime%" || exit /b 0
-git push
